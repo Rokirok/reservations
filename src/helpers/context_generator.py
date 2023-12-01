bc868f1483f3bd0410b394e6e1d1a8c4d05c43c7
Rokirok <19169658+Rokirok@users.noreply.github.com>
2023-12-02 00:25:50 +0200

    Formatting fixes

--- src/helpers/context_generator.py
+++ src/helpers/context_generator.py
@@ -1,7 +1,7 @@
 from src.database.entities.user import User
 
 
-def dashboard_context(user: User) -> { "is_admin": bool, "user": User }:
+def dashboard_context(user: User) -> {"is_admin": bool, "user": User}:
     return {
         "is_admin": user.role == 'admin',
         "user": user
